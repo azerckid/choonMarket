7d5b386918631719a4c8b11cbcc58a384d96dc28
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-01-13 11:35:01 +0900

    list and animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 export default function Home() {
     return (
-      <main className="bg-gray-100 sm:bg-red-100 md:bg-blue-100 lg:bg-green-100 xl:bg-yellow-100 2xl:bg-purple-100 h-screen flex items-center justify-center  p-4 dark:bg-gray-900">
-        <div className="w-full max-w-screen-sm p-4 rounded-2xl bg-white shadow-md dark:bg-gray-800 flex flex-col gap-2 items-center justify-center md:flex-row md:gap-4">
-            <input type="text" placeholder="Search" className="w-full p-2 rounded-full pl-4 bg-gray-200 text-gray-800 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            <button className="w-full bg-blue-500 text-white p-2 rounded-full hover:bg-pink-600 transition-all duration-300 active:scale-95 md:w-auto px-10">Search</button>
-        </div>
-      </main>
+        <main className="bg-gray-100 sm:bg-red-100 md:bg-blue-100 lg:bg-green-100 xl:bg-yellow-100 2xl:bg-purple-100 h-screen flex items-center justify-center  p-4 dark:bg-gray-900">
+            <div className="w-full max-w-screen-sm p-4 rounded-2xl bg-white shadow-md dark:bg-gray-800 flex flex-col gap-2">
+                {["Korean", "Japanese", "Chinese", "English", ""].map((person, index) =>
+                    <div key={index} className="flex items-center gap-5 odd:bg-gray-200 p-3 rounded-xl border-b-2 border-gray-300 last:border-b-0 last:pb-0">
+                        <div className="size-7 rounded-full bg-blue-400"></div>
+                        <span className="text-lg font-bold empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-600">{person}</span>
+                        <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+                            <span>{index}</span>
+                            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full animate-ping absolute"></div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </main>
     );
-  }
+}
