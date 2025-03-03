bdc0b76596b66bdc24cb7e1025dd043ad9f2217d
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-03-03 19:52:25 +0900

    #8.11 Edge Runtime

--- middleware.ts
+++ middleware.ts
@@ -22,5 +22,5 @@ export default async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/", "/profile", "/login", "/create-account", "/user/:path*"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
