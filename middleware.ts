c63975b88eaa921aa9fac1b87c8870bb1ff81c2f
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-03-05 14:49:01 +0900

    #8.12 authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib/session";
-import { cookies } from "next/headers";
+
+interface PublicOnlyUrls {
+    [key: string]: boolean;
+}
+
+const publicOnlyUrls: PublicOnlyUrls = {
+    "/login": true,
+    "/sms": true,
+    "/create-account": true,
+    "/": true,
+}
 
 export default async function middleware(request: NextRequest) {
     const session = await getSession();
-    // console.log(request.cookies.getAll());
-    // console.log(cookies());
-    // console.log(session);
-    const pathname = request.nextUrl.pathname;
-
-    if (pathname === "/profile") {
-        return NextResponse.redirect(new URL("/", request.url));
+    const exists = publicOnlyUrls[request.nextUrl.pathname];
+    if (!session.id) {
+        if (!exists) {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
+    } else {
+        if (exists) {
+            return NextResponse.redirect(new URL("/products", request.url));
+        }
     }
-
-    if (pathname === "/") {
-        const response = NextResponse.next();
-        response.cookies.set("middleware-cookie", "true");
-        return response;
-    }
-
 }
 
 export const config = {
