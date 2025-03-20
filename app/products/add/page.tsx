46bb65bc93f5d2080ab355332db2b98e32136f9e
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-03-20 15:14:11 +0900

    #11.1 Form Action

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -2,21 +2,54 @@
 
 import Button from "@/components/button";
 import Input from "@/components/input";
-import { PhotoIcon } from "@heroicons/react/24/solid";
+import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
-
+import { uploadProduct } from "./action";
 export default function AddProduct() {
     const [preview, setPreview] = useState("");
-    const onImageChange = () => { };
+    const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (!file) return;
+
+        // Check file size (2MB = 2 * 1024 * 1024 bytes)
+        if (file.size > 2 * 1024 * 1024) {
+            alert("이미지 크기는 2MB를 초과할 수 없습니다.");
+            event.target.value = ""; // Reset the input
+            return;
+        }
+
+        setPreview(URL.createObjectURL(file));
+    };
     return (
         <div>
-            <form className="p-5 flex flex-col gap-5">
+            <form
+                className="p-5 flex flex-col gap-5"
+                action={uploadProduct}>
                 <label
                     htmlFor="photo"
                     className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer"
+                    style={{
+                        backgroundImage: `url(${preview})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
                 >
-                    <PhotoIcon className="w-20" />
-                    <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+                    {preview ? (
+                        <div
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setPreview("");
+                            }}
+                            className="w-full h-full bg-black/50 text-white"
+                        >
+                            <XMarkIcon className="w-10" />
+                        </div>
+                    ) : (
+                        <>
+                            <PhotoIcon className="w-20" />
+                            <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+                        </>
+                    )}
                 </label>
                 <input
                     onChange={onImageChange}
